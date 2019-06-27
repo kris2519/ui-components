6fb73a9a638c1ca652d2f33296a11db0135f42a2
kris2519 <zhkris2519@gmail.com>
2019-06-27 19:43:44 +0300

    add flow; eslint; prettier

--- src/components/ui/svg/UiSvg.jsx
+++ src/components/ui/svg/UiSvg.jsx
@@ -1,5 +1,12 @@
+// @flow
+
 import React from 'react';
 import classNames from 'classnames';
-import style from './UiSvg.css';
-const UiSvg = ({klassName, icon}) => <svg className={classNames(klassName, "ui-svg", style.svgIco, style.svgIcoNestedTest)} ><use xlinkHref={`sprite.svg#${icon}`} /></svg>;
+
+const UiSvg = ({ klassName, icon }) => (
+  <svg className={classNames(klassName, 'ui-svg')}>
+    <use xlinkHref={`sprite.svg#${icon}`} />
+  </svg>
+);
+
 export default UiSvg;
